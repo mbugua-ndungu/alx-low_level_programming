679cd8f3a4afa25fdfc90fa535901501fc6be689
mbugua-ndungu <mia92jason@gmail.com>
2022-04-11 07:12:42 -0700

    s

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - assign a random number to the variable n each time it is executed
+ * it checks whether the number is zero, greater than 5 and less than 6 and not equal to 0
+ * Return: 0 exit code for the program
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	last = n % 10;
+	if(last > 5)
+		printif("Last digit of %d is %d and is greater than 5\n", n, last);
+	if(last == 0)
+                printif("Last digit of %d is %d and is 0\n", n, last);
+	if(last < 6 && last =! 0)
+                printif("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	return (0);
+}
