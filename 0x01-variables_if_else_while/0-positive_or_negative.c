2a0cfab479e9d3c1285868e9507ce9231d7d6fcd
mbugua-ndungu <mia92jason@gmail.com>
2022-04-11 05:57:13 -0700

    c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,15 +6,20 @@
 /*i e Function to determine +, -  */
 int main(void)
 {
+	/*i e Function to determine +, -  */
+
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	/*i e Function to determine +, -  */
 	if (n > 0)
 		printf("%d is positive\n", n);
+	/*i e Function to determine +, -  */
 	if (n < 0)
 		printf("%d is negative\n", n);
+	/*i e Function to determine +, -  */
 	if (n == 0)
 		printf("%d is zero\n", n);
 	return (0);
